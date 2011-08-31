8cf1ba723c902271d06ced2a8003aed2fa854576
Salvatore <dested@gmail.com>
2011-08-30 19:24:30 -0700

    Building of the final effects api.

--- javascript/SpokeEngine.js
+++ javascript/SpokeEngine.js
@@ -1,21 +1,73 @@
 ﻿$(function () {
     paper = Raphael("spokeBuilder");
+
+    box = paper.rect(470, 100, 240, 240);
+    box.attr({ 'stroke-width': 4 });
+    box.attr({ stroke: 'green' });
+    box.attr({ fill: 'red' });
+    points = [];
+    line = paper.path('');
+    line.attr({ 'stroke-width': 2 });
+    line.attr({ stroke: 'green' });
+    line.insertAfter(box);
+
+    box.click(function (ev) {
+        circ = paper.circle(ev.x, ev.y, 9);
+        circ.attr({ 'stroke-width': 2 });
+        circ.attr({ stroke: 'green' });
+        circ.attr({ fill: 'lightblue' });
+        circ.drag(function (dx, dy) {
+
+            this.attr({
+                cx: this.ox + dx,
+                cy: this.oy + dy
+            });
+
+            redrawPath();
+        }, function () {
+            this.ox = this.attr("cx");
+            this.oy = this.attr("cy");
+
+            this.attr({ fill: 'blue' });
+
+        }, function () {
+            this.attr({ fill: 'lightblue' });
+        });
+        points.push(circ);
+        redrawPath();
+    });
+
+    redrawPath = function () {
+        myPath = '';
+        if (points.length > 1) {
+            myPath = 'M' + points[0].attr('cx') + ',' + points[0].attr('cy');
+            for (jj = 1; jj < points.length; jj++) {
+                myPath += 'L' + points[jj].attr('cx') + ',' + points[jj].attr('cy');
+            }
+        }
+        line.attr({ path: myPath });
+    }
+
 });
 
 function SpokeEngine() {
+
+
+
+
     this.showPile = function (cards, left, top, scale, bend, maxWidth, dragStartEffects, dragEndEffects) {
         /*
         cards={
         cardIndex:0
         effects:[{
         type:'rect',
-        effectWidth:0
+        effectWidth:0,
         attributes:[{
-        type:'color','opactiy','rotation','location'
-        color:
-        amount:
-        x:
-        y:
+        type:'color','opactiy','rotation','location',
+        color:'black',
+        amount:0,
+        x:0,
+        y:0,
 
         animate:false,
         length:0,
@@ -36,8 +88,24 @@ function SpokeEngine() {
         }
 
 
+
+        for (var i = 0; i < dragStartEffects.length; i++) {
+            dragStartEffects[i].attributes['rotation'] = 0;
+        }
+        dragStartEffects.push({ type: 'original', attributes: { rotation: 0} });
+        dragEndEffects.push({ type: 'original', attributes: { startRotation: true} });
+
+
+        dragStartEffects.push({ type: 'original', attributes: { rotation: 0} });
+        dragEndEffects.push({ type: 'original', attributes: { startRotation: true} });
+
+
+
+
         for (var zs in cards) {
             var card = cards[zs];
+            card.effects.push({ type: 'original', attributes: { startRotation: true} });
+
             var c = paper.image("assets/cards/1" + card.cardIndex + ".gif", left, top + Math.abs(startAngle), 71, 96);
             c.scale(scale);
 
@@ -50,12 +118,11 @@ function SpokeEngine() {
 
             c.effects = [];
 
-            for (ia = 0; ia < card.effects.length; ia++) {
 
+            for (ia = 0; ia < card.effects.length; ia++) {
                 ef = card.effects[ia];
                 drawEffectOnCard(ef, c);
             }
-
 
             var start = function () {
                 if (this.isMoving) {
@@ -67,7 +134,6 @@ function SpokeEngine() {
                 this.animate({
                     rotation: 0
                 }, 700, ">");
-
 
                 for (var i = 0; i < dragStartEffects.length; i++) {
                     effect = drawEffectOnCard(dragStartEffects[i], this);
@@ -115,7 +181,6 @@ function SpokeEngine() {
 
         };
 
-            c.rotate(startAngle, true);
             c.drag(move, start, up);
             startAngle += angleIncrease;
             left = left + maxWidth / cards.length;
@@ -134,42 +199,51 @@ function drawEffectOnCard(effect, card, original) {
                 original = paper.rect(card.ox - effect.effectWidth, card.oy - effect.effectWidth, card.attr("width") + effect.effectWidth * 2, card.attr("height") + effect.effectWidth * 2, effect.effectWidth);
 
                 card.effects.push(original);
+                original.toBack();
             }
-            if (effect.attributes)
-                for (var i = 0; i < effect.attributes.length; i++) {
-                    at = effect.attributes[i];
-                    options = {};
-                    options['rotation'] = card.STARTANGLE;
-                    switch (at.type.toLowerCase()) {
-                        case 'color':
-                            options['fill'] = effect.color;
-                            break;
-                        case 'opacity':
-                            options['opacity'] = effect.amount;
-                            break;
-                        case 'rotation':
-                            options['rotation'] = card.STARTANGLE + effect.amount;
-                            break;
-                        case 'location':
-                            options['x'] = at.x;
-                            options['y'] = at.y;
-                            break;
-                    }
+            break;
 
-
-
-                    if (at.animate) {
-                        original.animate(options, at.length, at.extra, at.removeOnFinish);
-                    } else {
-                        original.attr(options);
-                    }
-
-                }
-
-            original.toBack();
-
+        case 'original':
+            if (!original) {
+                original = card;
+            }
             break;
     }
+
+
+    if (effect.attributes) {
+        for (var i = 0; i < effect.attributes.length; i++) {
+            at = effect.attributes[i];
+            options = {};
+            options['rotation'] = card.STARTANGLE;
+            switch (at.type.toLowerCase()) {
+                case 'color':
+                    options['fill'] = effect.color;
+                    break;
+                case 'opacity':
+                    options['opacity'] = effect.amount;
+                    break;
+                case 'rotation':
+                    options['rotation'] = card.STARTANGLE + effect.amount;
+                    break;
+                case 'startRotation':
+                    options['rotation'] = card.STARTANGLE;
+                    break;
+                case 'location':
+                    options['x'] = at.x;
+                    options['y'] = at.y;
+                    break;
+            }
+
+            if (at.animate) {
+                original.animate(options, at.length, at.extra, at.removeOnFinish);
+            } else {
+                original.attr(options);
+            }
+
+        }
+    }
+
 }
 
 function drawText(x, y, size, string, attr) {
